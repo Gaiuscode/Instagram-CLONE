304d25437733eb88b0e7cb8a1745fa1b5b6701e0
GaiusCode <boluwatifegaius@gmail.com>
2022-10-21 21:54:53 +0100

    fixed it up

--- js/user.js
+++ js/user.js
@@ -20,7 +20,7 @@ preArrow.addEventListener('click', () => {
 const userPosts = [...document.querySelectorAll('.post-container .post')];
 let popupPost = document.querySelector('.post-popup');
 let closePopup = document.querySelector('.close-overlay');
-let popupPostImg = document.querySelector('.post-popup .post .post-img-container .img')
+let popupPostImg = document.querySelector('.post-popup .post .post-img-container img')
 
 userPosts.map(post => {
     post.addEventListener('click', () => {
@@ -28,5 +28,9 @@ userPosts.map(post => {
         popupPostImg.src = img.src;
         popupPost.classList.add('show')
     })
-}
-)
+})
+
+closePopup.addEventListener('click', () => popupPost.classList.remove('show'));
+
+posts = [...document.querySelectorAll('.post-popup .post')]
+posts.map(post => addInterationsToPost(post));
