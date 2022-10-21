204f9727f080d0b2202a558aa516687497182ecc
GaiusCode <boluwatifegaius@gmail.com>
2022-10-21 21:18:57 +0100

    fixed it up

--- js/user.js
+++ js/user.js
@@ -1,4 +1,16 @@
-// suggestion
+// suggestion carousel slider
 
-let userCardContainer = document.querySelector('suggestion-container');
-let nxtArrow = document.querySelector('nxt-arrow');
+let userCardContainer = document.querySelector('.suggestion-container');
+let nxtArrow = document.querySelector('.nxt-arrow');
+let preArrow = document.querySelector('.pre-arrow');
+
+let containerDimensions = userCardContainer.getBoundingClientRect();
+let containerWidth = containerDimensions.width;
+
+nxtArrow.addEventListener('click', () => {
+    userCardContainer.scrollLeft += containerWidth;
+})
+
+preArrow.addEventListener('click', () => {
+    userCardContainer.scrollLeft -= containerWidth;
+})
