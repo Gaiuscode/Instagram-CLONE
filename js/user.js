f8388e31e879d7ea49790cc14db63db58d534333
GaiusCode <boluwatifegaius@gmail.com>
2022-10-21 21:40:21 +0100

    fixed it up

--- js/user.js
+++ js/user.js
@@ -14,3 +14,19 @@ nxtArrow.addEventListener('click', () => {
 preArrow.addEventListener('click', () => {
     userCardContainer.scrollLeft -= containerWidth;
 })
+
+// popup post
+
+const userPosts = [...document.querySelectorAll('.post-container .post')];
+let popupPost = document.querySelector('.post-popup');
+let closePopup = document.querySelector('.close-overlay');
+let popupPostImg = document.querySelector('.post-popup .post .post-img-container .img')
+
+userPosts.map(post => {
+    post.addEventListener('click', () => {
+        let img = post.querySelector('.post-img');
+        popupPostImg.src = img.src;
+        popupPost.classList.add('show')
+    })
+}
+)
